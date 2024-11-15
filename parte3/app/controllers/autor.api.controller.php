51ce8f1ef7ea15e0266ca17948559747ebcefe1a
gonzasola6 <146140655+gonzasola6@users.noreply.github.com>
2024-11-15 16:55:04 -0300

    Update autor.api.controller.php

--- parte3/app/controllers/autor.api.controller.php
+++ parte3/app/controllers/autor.api.controller.php
@@ -37,4 +37,82 @@ class AutorApiController{
         return $this->view->response($autor);
     }
 
+    public function delete($req, $res){
+        $id= $req->params->id;
+
+        $autor= $this->model->getAutor($id);
+
+        if(!$autor){
+            return $this->view->response("El autor con el id=$id no existe", 404);
+        }
+        $this->model->eraseAutor($id);
+        $this->view->response("El autor con el id =$id se eliminó con éxito");
+
+    }
+
+    public function add($req,$res){
+        //valido los datos
+        if(empty($req->body->nombre)){
+            return $this->view->response('Faltan completar el nombre',400);
+        }
+        if(empty($req->body->pais)){
+            return $this->view->response('Falta completar pais',400);
+        }
+        if(empty($req->body->cantAlbumes)){
+            return $this->view->response('Faltan completar la cantidad de albumes',400);
+        }
+       
+        //obtengo los datos del body del request
+        $nombre= $req->body->nombre;
+        $pais= $req->body->pais;
+        $cantAlbumes= $req->body->cantAlbumes;
+        
+        //inserto los datos$
+        $id= $this->model->insertAutor($nombre, $pais, $cantAlbumes);
+
+        if(!$id){
+            return $this->view->response ("Error al insertar autor", 500);
+        }
+            // buena práctica devuelvo la tarea insertada
+            $autor= $this->model->getAutor($id);
+
+            return $this->view->response($autor,201);
+
+    }
+
+    public function update($req, $res){
+        $id= $req->params->id;//qué tarea quiero actualizar
+
+        $autor= $this->model->getAutor($id);
+
+        if(!$autor){ //verifico que exista
+            return $this->view->response("La tarea con el id= $id no existe", 404);
+        }
+
+        if(empty($req->body->nombre)){
+            return $this->view->response('Faltan completar el nombre',400);
+        }
+        if(empty($req->body->pais)){
+            return $this->view->response('Falta completar pais',400);
+        }
+        if(empty($req->body->cantAlbumes)){
+            return $this->view->response('Faltan completar la cantidad de albumes',400);
+        }
+       
+        //obtengo los datos del body del request
+        $nombre= $req->body->nombre;
+        $pais= $req->body->pais;
+        $cantAlbumes= $req->body->cantAlbumes;
+
+        //modifico si pasa las validaciones
+
+        $this->model->updateAutor ($id,$nombre, $pais, $cantAlbumes);
+
+        //obtengo la autor modificada y la muestro
+        $autor= $this->model->getAutor($id);
+        $this->view->response ($autor, 200);
+
+        
+
+    }
 }

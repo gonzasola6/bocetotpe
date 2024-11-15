bfacd15a05d01c28671d9802d81a3a3eddea9ad7
gonzasola6 <146140655+gonzasola6@users.noreply.github.com>
2024-11-15 16:55:51 -0300

    Update router.php

--- parte3/router.php
+++ parte3/router.php
@@ -11,11 +11,15 @@
     #                 endpoint        verbo     controller             metodo
     $router->addRoute('albumes'      , 'GET',    'AlbumApiController',   'getAll');
     $router->addRoute('albumes/:id'  , 'GET',    'AlbumApiController',   'get');
+    $router->addRoute ('albumes/:id', 'DELETE',  'AlbumApiController',   'delete');
+    $router->addRoute ('albumes'    ,  'POST',    'AlbumApiController',   'add');
+    $router->addRoute ('albumes/:id', 'PUT',    'AlbumApiController',    'update');
+    
     $router->addRoute('autores'      , 'GET',    'AutorApiController',   'getAll');
     $router->addRoute('autores/:id'  , 'GET',    'AutorApiController',   'get');
-    $router->addRoute ('albumes/:id', 'DELETE',  'AlbumApiController',   'delete');
-    $router->addRoute ('albumes'    ,     'POST',    'AlbumApiController',   'add');
-    $router->addRoute ('albumes/:id', 'PUT',    'AlbumApiController',    'update');
+    $router->addRoute ('autores/:id', 'DELETE',  'AutorApiController',   'delete');
+    $router->addRoute ('autores'    ,  'POST', 'AutorApiController',   'add');
+    $router->addRoute ('autores/:id', 'PUT',    'AutorApiController',    'update');
 
 
     $router->route($_GET['resource'], $_SERVER['REQUEST_METHOD']);

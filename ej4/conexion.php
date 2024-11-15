43d1a291556e1c4491ccb703d6bc94a453c4ca2c
gonzasola6 <146140655+gonzasola6@users.noreply.github.com>
2024-11-15 15:27:58 -0300

    Delete ej4 directory

--- ej4/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db = new PDO('mysql:host=localhost;dbname=ej3tp3;charset-utf8','root','');
-
-$sentencia = $db->prepare("SELECT * FROM pagos");
-$sentencia->execute();
-
-$pagos = $sentencia->fetchAll(PDO::FETCH_OBJ);
